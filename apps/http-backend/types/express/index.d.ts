94e10f43bfe303dde748493232cbad03c957eb58
Saurabh Kumar <sbhk4325@gmail.com>
2026-01-24 20:40:36 +0530

    remove some folder

--- apps/http-backend/types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-// types/express/index.d.ts
-
-import { Request } from "express-serve-static-core";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    userId: string;
-  }
-}
--- apps/web/Config.ts
+++
@@ -1,2 +0,0 @@
-export const HTTP_BACKEND = "https://sketchkaro-jovi.onrender.com"
-export const WS_URL = "https://sketchkaro-ws-backend.onrender.com"
